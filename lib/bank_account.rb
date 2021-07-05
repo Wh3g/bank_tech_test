f60568bf37f48b6d3536976536a5a51e0bad24c1
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-07-05 14:32:57 +0100

    saves all transactions and added #bank_statement method

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,15 +8,27 @@ class BankAccount
 
   def deposit(amount, date)
     @balance += amount
-    transactions << "#{date} || #{decimal_places(amount)} || || #{decimal_places(@balance)}"
+    transactions << "#{date_format(date)} || #{decimal_places(amount)} || || #{decimal_places(@balance)}"
   end
 
-  def withdraw(amount)
+  def withdraw(amount, date)
     @balance -= amount
+    transactions << "#{date_format(date)} || || #{decimal_places(amount)} || #{decimal_places(@balance)}"
+  end
+
+  def bank_statement
+    "date || credit || debit || balance\n#{@transactions.reverse.join("\n")}"
   end
 
   private
   def decimal_places(number)
     '%.2f' % number
+  end
+
+  def date_format(date)
+    2.times do
+      date["-"] = "/"
+    end
+    date
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -8,7 +8,8 @@ describe BankAccount do
 
   context "with a deposit" do
     before do
-      subject.deposit(100.00, "10/01/2012")
+      subject.deposit(100.00, "10-01-2012")
+      @deposit_str = "10/01/2012 || 100.00 || || 100.00"
     end
 
     describe "#deposit" do
@@ -17,14 +18,30 @@ describe BankAccount do
      end
 
       it "saves the transaction" do
-        expect(subject.transactions[-1]).to eq("10/01/2012 || 100.00 || || 100.00")
+        expect(subject.transactions[-1]).to eq(@deposit_str)
       end
     end
 
-    describe "#withdraw" do
-      it "decreases the bank balance" do
-        subject.withdraw(50)
-        expect(subject.balance).to eq 50
+    context "with a withdrawal" do
+      before do
+        subject.withdraw(50, "10-01-2012")
+        @withdrawal_str = "10/01/2012 || || 50.00 || 50.00"
+      end
+
+      describe "#withdraw" do
+        it "decreases the bank balance" do
+          expect(subject.balance).to eq 50
+        end
+
+        it "saves the transaction" do
+          expect(subject.transactions[-1]).to eq(@withdrawal_str)
+        end
+      end
+
+      describe "#bank_statement" do
+        it "returns a record of transactions" do
+          expect(subject.bank_statement).to eq("date || credit || debit || balance\n#{@withdrawal_str}\n#{@deposit_str}")
+        end
       end
     end
   end
