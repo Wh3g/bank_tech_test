6049ff7af48c599da93297bb5c9309af07b8d6d1
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-07-05 12:24:24 +0100

    saves deposit transaction

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,15 +1,22 @@
 class BankAccount
-  attr_reader :balance
+  attr_reader :balance, :transactions
   
   def initialize
-    @balance = 0
+    @balance = 0.00
+    @transactions = []
   end
 
-  def deposit(amount)
+  def deposit(amount, date)
     @balance += amount
+    transactions << "#{date} || #{decimal_places(amount)} || || #{decimal_places(@balance)}"
   end
 
   def withdraw(amount)
     @balance -= amount
+  end
+
+  private
+  def decimal_places(number)
+    '%.2f' % number
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -6,18 +6,26 @@ describe BankAccount do
     end
   end
 
-  describe "#deposit" do
-    it "increases the balance value" do
-      subject.deposit(100)
-      expect(subject.balance).to eq 100
+  context "with a deposit" do
+    before do
+      subject.deposit(100.00, "10/01/2012")
     end
-  end
 
-  describe "#withdraw" do
-    it "decreases the bank balance" do
-      subject.deposit(100)
-      subject.withdraw(50)
-      expect(subject.balance).to eq 50
+    describe "#deposit" do
+      it "increases the balance value" do
+       expect(subject.balance).to eq 100
+     end
+
+      it "saves the transaction" do
+        expect(subject.transactions[-1]).to eq("10/01/2012 || 100.00 || || 100.00")
+      end
+    end
+
+    describe "#withdraw" do
+      it "decreases the bank balance" do
+        subject.withdraw(50)
+        expect(subject.balance).to eq 50
+      end
     end
   end
 end
