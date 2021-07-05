e6620c5454071eb27e8707496fc413e52b17bd8d
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-07-05 11:43:57 +0100

    added deposit functionality

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -5,4 +5,11 @@ describe BankAccount do
       expect(subject.balance).to eq 0
     end
   end
+
+  describe "#deposit" do
+    it "increases the balance value" do
+      subject.deposit(100)
+      expect(subject.balance).to eq 100
+    end
+  end
 end
